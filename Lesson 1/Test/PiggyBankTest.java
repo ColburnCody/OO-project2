86d83135f1ae409a2858aa18bbfb990b2c35e49d
Cody <ccolburn@uco.edu>
2020-08-19 10:27:54 -0500

    Implemented PIggyBankTest

---
+++ Lesson 1/Test/PiggyBankTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import model.Coin;
+
+public class PiggyBankTest {
+
+    public static void main(String[] args){
+        Coin c1 = new Coin(5);
+        assert c1.getValue() == 5;
+
+        Coin c2 = new Coin(10);
+        assert c2.getValue() == 10;
+    }
+    
+}
