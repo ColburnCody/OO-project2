3a923d11926b1bd0d5263a201973a54784f01609
Cody <ccolburn@uco.edu>
2020-08-31 09:57:42 -0500

    Added elements to BaseBallGamePanel

--- Lesson 2/view/BaseballGamePanel.java
+++ Lesson 2/view/BaseballGamePanel.java
@@ -10,6 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import model.BaseballGame;
+
 
 
 public class BaseballGamePanel {
@@ -20,8 +22,10 @@ public class BaseballGamePanel {
     private JTextField gameKeyField = new JTextField();
     private JTextField guessField = new JTextField();
     private JButton[] digitButtons;
-    private JButton playButton = new JButton("Play Ball");
+    private JButton playButton = new JButton("Play Ball~~");
     private JButton exitButton = new JButton("Exit");
+
+    private BaseballGame baseball;
 
     public BaseballGamePanel(JFrame window){
         this.window = window;
@@ -42,6 +46,53 @@ public class BaseballGamePanel {
 
         canvas = new BaseballCanvas(this);
         cp.add(BorderLayout.CENTER, canvas);
+
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new GridLayout(4, 3));
+        digitButtons = new JButton[10];
+        for(int i = 0; i < 10; i++){
+            digitButtons[i] = new JButton("" + i);
+            southPanel.add(digitButtons[i]);
+        }
+        for(var b: digitButtons){
+            b.setEnabled(false);
+        }
+        southPanel.add(playButton);
+        southPanel.add(exitButton);
+        cp.add(BorderLayout.SOUTH, southPanel);
+
+    }
+
+    public BaseballGame getBaseballGame(){
+        return baseball;
+    }
+
+    public JFrame getWindow(){
+        return window;
+    }
+
+    public BaseballCanvas getCanvas(){
+        return canvas;
+    }
+
+    public JTextField getGameKey(){
+        return gameKeyField;
+    }
+
+    public JTextField getGuessField(){
+        return guessField;
+    }
+
+    public JButton[] getDigitButtons(){
+        return digitButtons;
+    }
+
+    public JButton getPlayButton(){
+        return playButton;
+    }
+
+    public JButton getExitButton(){
+        return exitButton;
     }
     
 }
