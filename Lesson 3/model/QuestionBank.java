197cd2a198822924efcc0b4f6d3352707b0f0670
Cody <ccolburn@uco.edu>
2020-09-15 09:55:01 -0500

    added menu screen

--- Lesson 3/model/QuestionBank.java
+++ Lesson 3/model/QuestionBank.java
@@ -15,6 +15,24 @@ public class QuestionBank {
             "In what city is UCO located?", 
             "Edmond"
             ));
+        var c1 = new ChoiceQuestion(
+            "What is the capital city of Oklahoma?",
+            "2"
+        );
+        c1.addChoices("Norman");
+        c1.addChoices("Oklahoma City");
+        c1.addChoices("Edmond");
+        c1.addChoices("Tulsa");
+        database.add(c1);
+        var c2 = new ChoiceQuestion(
+            "Which of the following is not a Java keyword?",
+            "4"
+        );
+        c2.addChoices("int");
+        c2.addChoices("for");
+        c2.addChoices("instanceof");
+        c2.addChoices("String");
+        database.add(c2);
     }
 
 }
