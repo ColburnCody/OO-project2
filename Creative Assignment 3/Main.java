f988e40bd8b9b6ebc42c969bddcc2e19debc0f08
Cody <ccolburn@uco.edu>
2020-09-24 13:28:11 -0500

    initial commit

--- Creative Assignment 3/Main.java
+++ Creative Assignment 3/Main.java
@@ -1,20 +1,16 @@
 import javax.swing.JFrame;
 
-import view.GardenScreen;
-
 public class Main{
-
     public static void main(String[] args){
-
-        javax.swing.JFrame window = new JFrame();
+        JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocation(400, 100);
+        window.setTitle("Maze Game");
 
-        var garden = new GardenScreen(window);
-        garden.init();
+        var menu = new MenuScreen(window);
+        menu.init();
 
         window.pack();
         window.setVisible(true);
-
     }
 }
