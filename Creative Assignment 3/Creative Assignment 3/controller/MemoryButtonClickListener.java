4e489e6bcc38c3ff9d12a439efd81d4966e020b3
Cody <ccolburn@uco.edu>
2020-09-24 10:45:41 -0500

    began easy difficulty

--- Creative Assignment 3/Creative Assignment 3/controller/MemoryButtonClickListener.java
+++ Creative Assignment 3/Creative Assignment 3/controller/MemoryButtonClickListener.java
@@ -4,15 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
+import model.Suit;
 import view.MemoryPanel;
 
 public class MemoryButtonClickListener implements ActionListener, MouseListener {
 
     private MemoryPanel panel;
+
+    private ArrayList<Suit> deck;
+    private Random r;
 
     public MemoryButtonClickListener(MemoryPanel panel){
         this.panel = panel;
@@ -24,6 +30,14 @@ public class MemoryButtonClickListener implements ActionListener, MouseListener 
         if(button == panel.getNewButton()){
             if(!panel.getEasyButton().isSelected() && !panel.getMediumButton().isSelected() && !panel.getHardButton().isSelected()){
                 JOptionPane.showMessageDialog(null, "Please select a difficulty", "Error", JOptionPane.CANCEL_OPTION);
+            }else{
+                if(panel.getEasyButton().isSelected()){
+                    r = new Random();
+                    for(int i = 0; i < 2; i++){
+                        int s = r.nextInt(4);
+                        int c = r.nextInt(2);
+                    }
+                }
             }
         }
 
