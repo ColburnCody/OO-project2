6073d69182f4ad2e3849b49ae3ac695be1605b5a
Cody <ccolburn@uco.edu>
2020-10-01 12:39:33 -0500

    initial commit

--- Creative Assignment 3/Main.java
+++ Creative Assignment 3/Main.java
@@ -1,11 +1,12 @@
 import javax.swing.JFrame;
 
+import view.MenuScreen;
+
 public class Main{
     public static void main(String[] args){
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocation(400, 100);
-        window.setTitle("Maze Game");
 
         var menu = new MenuScreen(window);
         menu.init();
