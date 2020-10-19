7d59b700931a6523991180bf70505d611c605397
Cody <ccolburn@uco.edu>
2020-10-19 08:58:47 -0500

    initial commit

---
+++ Lesson 6/view/MyCanvas.java
@@ -0,0 +1,26 @@
+package view;
+
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class MyCanvas extends JPanel {
+
+    private GameBoard gameBoard;
+
+    public MyCanvas(GameBoard gameBoard, int width, int height){
+        this.gameBoard = gameBoard;
+        setPreferredSize(new Dimension(width, height));
+        setBackground(Color.black);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+    }
+    
+}
